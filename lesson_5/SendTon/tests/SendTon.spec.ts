322238612626678bc862e0c632f779debced94db
Nikita Pilets <pileznikita99@gmail.com>
2023-12-14 16:50:22 +0300

    Withdraw

--- lesson_5/SendTon/tests/SendTon.spec.ts
+++ lesson_5/SendTon/tests/SendTon.spec.ts
@@ -1,6 +1,6 @@
 import { Blockchain, SandboxContract, TreasuryContract } from '@ton/sandbox';
 import { toNano } from '@ton/core';
-import { SendTon } from '../wrappers/SendTon';
+import { SendTon, Withdraw } from '../wrappers/SendTon';
 import '@ton/test-utils';
 
 describe('SendTon', () => {
@@ -32,10 +32,103 @@ describe('SendTon', () => {
             deploy: true,
             success: true,
         });
+
+        await sendTon.send(
+            deployer.getSender(),
+            {
+                value: toNano("500")
+            },
+            null
+        )
     });
 
     it('should deploy', async () => {
         // the check is done inside beforeEach
         // blockchain and sendTon are ready to use
     });
+
+    it("should withdraw all", async()=>{
+        const user = await blockchain.treasury('user');
+
+        const balanceBeforeUser = await user.getBalance()
+
+        await sendTon.send(user.getSender(), {
+            value: toNano("0.2")
+        }, 'withdraw all')
+
+        const balanceAfterUser = await user.getBalance()
+
+        expect(balanceBeforeUser).toBeGreaterThanOrEqual(balanceAfterUser)
+
+        const balanceBeforeDeployer = await deployer.getBalance()
+
+        await sendTon.send(deployer.getSender(), {
+            value: toNano("0.2")
+        }, 'withdraw all')
+
+        const balanceAfterDeployer = await deployer.getBalance()
+
+        expect(balanceAfterDeployer).toBeGreaterThan(balanceBeforeDeployer)
+    })
+
+    it("should withdraw safe", async()=>{
+        const user = await blockchain.treasury('user');
+
+        const balanceBeforeUser = await user.getBalance()
+
+        await sendTon.send(user.getSender(), {
+            value: toNano("0.2")
+        }, 'withdraw safe')
+
+        const balanceAfterUser = await user.getBalance()
+
+        expect(balanceBeforeUser).toBeGreaterThanOrEqual(balanceAfterUser)
+
+        const balanceBeforeDeployer = await deployer.getBalance()
+
+        await sendTon.send(deployer.getSender(), {
+            value: toNano("0.2")
+        }, 'withdraw safe')
+
+        const balanceAfterDeployer = await deployer.getBalance()
+
+        expect(balanceAfterDeployer).toBeGreaterThan(balanceBeforeDeployer)
+
+        const contractBalance = await sendTon.getBalance()
+
+        expect(contractBalance).toBeGreaterThan(0n)
+    })
+
+    it("should withdraw message", async()=>{
+        const message: Withdraw = {
+            $$type: 'Withdraw',
+            amount: toNano("150")
+        }
+
+        const user = await blockchain.treasury('user');
+
+        const balanceBeforeUser = await user.getBalance()
+
+        await sendTon.send(user.getSender(), {
+            value: toNano("0.2")
+        }, message)
+
+        const balanceAfterUser = await user.getBalance()
+
+        expect(balanceBeforeUser).toBeGreaterThanOrEqual(balanceAfterUser)
+
+        const balanceBeforeDeployer = await deployer.getBalance()
+
+        await sendTon.send(deployer.getSender(), {
+            value: toNano("0.2")
+        }, message)
+
+        const balanceAfterDeployer = await deployer.getBalance()
+
+        expect(balanceBeforeDeployer + toNano("150")).toBeGreaterThanOrEqual(balanceAfterDeployer)
+
+        const contractBalance = await sendTon.getBalance()
+
+        expect(contractBalance).toBeGreaterThan(0n)
+    })
 });
